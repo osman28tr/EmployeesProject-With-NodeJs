5840375c4e46c4149512800f33426176c0d93b46
osman28tr <osmantonbul2001@gmail.com>
2023-08-15 16:50:55 +0300

    added class of request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-console.log("Test");
-console.log("test2");
-console.log("test3");
+import { Request } from "./request";
+//element seçimleri
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeeList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees");
+
+//request.get().then(employees=>console.log(employees)).catch(err=>console.log(err));
+//request.post({name:"osman tonbul3",department:"bilişim",salary:15000}).then(response=>console.log(response)).catch(err=>console.log(err));
+//request.put(1,{name:"osman tonbul4",department:"bilişim",salary:15000}).then(response=>console.log(response)).catch(err=>console.log(err));
+request.delete(39).then(response=>console.log(response)).catch(err=>console.log(err));
+
